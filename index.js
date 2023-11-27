379b5e6c426c33da6f017ef753bb72fa63f4c2ef
IngenAningVadJagSkaSkriva <123164118+IngenAningVadJagSkaSkriva@users.noreply.github.com>
2023-11-27 08:30:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 let w = window.open('','','HEIGHT=1,WIDTH=1');
-w.document.write('<script src="stuff/index2.js"></script>');
+w.document.write('<script src="index2.js"></script>');
 open('index2.html');
 close();
